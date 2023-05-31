62834e4d757de02e45a88f95471d460d230cfd72
stanislawix <63518839+stanislawix@users.noreply.github.com>
2023-05-31 01:38:36 +0000

    Formularze

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,5 +1,24 @@
 class TrainingsController < ApplicationController
-    def index
-      @trainings = Training.all
+  before_action :authenticate_user!
+  def index
+    @trainings = Training.all
+  end
+  def new
+    @training = Training.new
+  end
+
+  def create
+    @training = current_user.trainings.build(training_params)
+    if @training.save
+      redirect_to root_path, notice: 'Trening dodany pomyślnie.'
+    else
+      render :new
     end
   end
+
+  private
+
+  def training_params
+    params.require(:training).permit(:liczba_pompek, :data)
+  end
+end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,13 @@
 class Training < ApplicationRecord
+  validate :valid_date
+
+  def valid_date
+    if data.present? && data > Date.current
+      errors.add(:data, "Nie możesz dodawać treningów na przyszłe dni.")
+    end
+  end
+  
   belongs_to :user
+  validates :data, presence: true
+  validates :data, uniqueness: { scope: :user_id, message: 'Możesz dodać tylko jeden trening danego dnia.' }
 end
